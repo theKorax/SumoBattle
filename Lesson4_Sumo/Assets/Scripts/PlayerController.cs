bfeaf326648409a4d1678abd44b9b6fe45674cd5
theKorax <zikoramadin@gmail.com>
2023-09-08 11:59:02 +0100

    Updated PlayerController.cs
    Modified the script so now the player moves in the direction that the camera is facing

--- Lesson4_Sumo/Assets/Scripts/PlayerController.cs
+++ Lesson4_Sumo/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * forwardInput * speed);
+        // moves the player in the direction that our camera is facing
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
 }
