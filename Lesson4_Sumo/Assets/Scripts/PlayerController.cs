02faecc0827733c894a307dd543b54988a779f08
theKorax <zikoramadin@gmail.com>
2023-09-08 11:49:06 +0100

    Updated PlayerController.cs
    The script adds a force to the player anytime the up or down arrow keys are pressed in order to move the player forward.

--- Lesson4_Sumo/Assets/Scripts/PlayerController.cs
+++ Lesson4_Sumo/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * forwardInput * speed);
     }
 }
