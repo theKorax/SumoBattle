d492cd09776561dac1c91afe4c92a8e0c5c52c0b
theKorax <zikoramadin@gmail.com>
2023-09-08 14:03:56 +0100

    Updated SpawnManager.cs
    Makes the enemyPrefab spawn in a specific location when the game starts

--- Lesson4_Sumo/Assets/Scripts/SpawnManager.cs
+++ Lesson4_Sumo/Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
