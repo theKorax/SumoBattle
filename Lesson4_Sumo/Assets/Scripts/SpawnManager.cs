6515d987964562f93eecfe27d5572f817515a7d2
theKorax <zikoramadin@gmail.com>
2023-09-08 16:03:27 +0100

    Updated SpawnManager.cs
    Updated the script to spawn more enemies if all the enemies on our platform fall off

--- Lesson4_Sumo/Assets/Scripts/SpawnManager.cs
+++ Lesson4_Sumo/Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +24,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if(enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
